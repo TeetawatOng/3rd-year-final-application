65eb284934ad06f6ccebad2010bf93ca1e20ae0e
Nuttawat28 <152236063+Nuttawat28@users.noreply.github.com>
2025-03-27 11:37:32 +0700

    add achievement and score

--- app/src/main/java/com/example/final_application/Achievement.java
+++ app/src/main/java/com/example/final_application/Achievement.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class achievement extends AppCompatActivity {
+public class Achievement extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
